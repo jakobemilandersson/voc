1c509a8f81191660895dd3a642acc0a948e1ff10
lucatonile <jaamongo@gmail.com>
2018-09-13 22:08:33 +0200

    added test cases for type and value errors. fixed java code for handling boolean inputs. string inputs leading to type errors not handled yet.

--- python/common/python/calendar.java
+++ python/common/python/calendar.java
@@ -11,11 +11,27 @@ public class calendar extends org.python.types.Module {
             args = {"x", "y"}
     )
     public static org.python.Object leapdays(org.python.Object x, org.python.Object y) {
-        return org.python.types.Int.getInt((Math.floorDiv(((org.python.types.Int) y).value-1, org.python.types.Int.getInt(4).value) - 
-        	Math.floorDiv(((org.python.types.Int) x).value-1, org.python.types.Int.getInt(4).value)) -
-        	(Math.floorDiv(((org.python.types.Int) y).value-1, org.python.types.Int.getInt(100).value) - 
-        	Math.floorDiv(((org.python.types.Int) x).value-1, org.python.types.Int.getInt(100).value)) +
-        	(Math.floorDiv(((org.python.types.Int) y).value-1, org.python.types.Int.getInt(400).value) - 
-        	Math.floorDiv(((org.python.types.Int) x).value-1, org.python.types.Int.getInt(400).value)));
+		if(x instanceof org.python.types.Str || y instanceof org.python.types.Str){
+			throw new org.python.exceptions.TypeError(
+				"unsupported operand type(s) for -=: 'str' and 'int'");
+		}
+
+		long x_ = ((org.python.types.Int) x.__int__()).value-1;
+		long y_ = ((org.python.types.Int) y.__int__()).value-1;
+		
+		org.python.types.Int result = org.python.types.Int.getInt((Math.floorDiv(y_, org.python.types.Int.getInt(4).value) - 
+				Math.floorDiv(x_, org.python.types.Int.getInt(4).value)) -
+				(Math.floorDiv(y_, org.python.types.Int.getInt(100).value) - 
+				Math.floorDiv(x_, org.python.types.Int.getInt(100).value)) +
+				(Math.floorDiv(y_, org.python.types.Int.getInt(400).value) - 
+				Math.floorDiv(x_, org.python.types.Int.getInt(400).value)));
+		
+		if(x instanceof org.python.types.Float || y instanceof org.python.types.Float){
+			return ((org.python.types.Float) result.__float__());
+		}
+		else{
+			return result;
+		}
+        
    }
 }
--- tests/stdlib/test_calendar.py
+++ tests/stdlib/test_calendar.py
@@ -4,21 +4,47 @@ from unittest import expectedFailure
 
 class CalendarModuleTests(TranspileTestCase):
 
-	#@expectedFailure
+	
 	def test_leapdays(self):
 	        self.assertCodeExecution("""
 	        	import calendar
 	        	print(calendar.leapdays(2015,2018))
 	        	""")
-	#@expectedFailure
 	def test_leapdays_double(self):
 	        self.assertCodeExecution("""
 	        	import calendar
 	        	print(calendar.leapdays(2015.4,2018))
 	        	""")
-	#@expectedFailure
 	def test_leapdays_descending(self):
 	        self.assertCodeExecution("""
 	        	import calendar
 	        	print(calendar.leapdays(2018,2015))
+	        	""")
+	def test_leapdays_booltrue(self):
+	        self.assertCodeExecution("""
+	        	import calendar
+	        	print(calendar.leapdays(2015,True))
+	        	""")
+	def test_leapdays_boolfalse(self):
+	        self.assertCodeExecution("""
+	        	import calendar
+	        	print(calendar.leapdays(2015,False))
+	        	""")
+	@expectedFailure
+	def test_leapdays_str1(self):
+	        self.assertCodeExecution("""
+	        	import calendar
+	        	print(calendar.leapdays(2015,'abc'))
+	        	""")
+	@expectedFailure
+	def test_leapdays_str2(self):
+	        self.assertCodeExecution("""
+	        	import calendar
+	        	print(calendar.leapdays('abc',2015))
+	        	""")
+	@expectedFailure
+	def test_leapdays_str3(self):
+	        self.assertCodeExecution("""
+	        	import calendar
+	        	print(calendar.leapdays('cba','abc'))
 	        	""")
