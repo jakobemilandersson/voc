63307c2232fe5850683ed1f61087bc09504a95ab
Jakob Andersson <jaan0006@student.uu.se>
2018-09-25 13:36:41 +0200

    Added more tests: for float, functions etc

--- tests/stdlib/test_datetime.py
+++ tests/stdlib/test_datetime.py
@@ -6,14 +6,109 @@ from ..utils import TranspileTestCase
 class TimeModuleTests(TranspileTestCase):
 
     #######################################################
-    @expectedFailure
-    def test_timedelta(self):
+    def test_timedelta_con1(self):
         self.assertCodeExecution("""
             from datetime import timedelta
             t1 = timedelta(days=1, hours=10, minutes=15, seconds=1)
-            t2 = timedelta(weeks=1, days=2, hours=-5, seconds=0)
+            print (t1)
+        """)
+
+    def test_timedelta_con2(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta()
+            print (t1)
+        """)
+
+    def test_timedelta_con3(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(1)
+            print (t1)
+        """)
+
+    def test_timedelta_con4(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(1,0,10)
+            print (t1)
+        """)
+
+    def test_timedelta_con_float1(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(1.2)
+            print (t1)
+        """)
+
+    def test_timedelta_con_float2(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(1.3)
+            print (t1)
+        """)
+
+    def test_timedelta_con_float3(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(0, 1.2)
+            print (t1)
+        """)
+
+    def test_timedelta_con_float4(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(0, 0, 1.2)
+            print (t1)
+        """)
+
+    def test_timedelta_con_float5(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(0, 0, 0, 1.2)
+            print (t1)
+        """)
+
+    def test_timedelta_con_float6(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(0, 0, 0, 0, 1.2)
+            print (t1)
+        """)
+    
+    def test_timedelta_con_float7(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(0, 0, 0, 0, 0, 1.2)
+            print (t1)
+        """)
+    
+    def test_timedelta_con_float8(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(0, 0, 0, 0, 0, 0, 1.2)
+            print (t1)
+        """)
+
+    def test_timedelta_con_float9(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(days=1.2)
+            print (t1)
+        """)
+
+    
+    def test_timedelta_mul1(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(days=1, hours=10, minutes=15, seconds=1)
             i = 5
-            print (t1 + t2)
-            print (i * t1)
-            print (t2.total_seconds())
+            print (t1 * 1)
+        """)
+
+    def test_timedelta_tot_sec1(self):
+        self.assertCodeExecution("""
+            from datetime import timedelta
+            t1 = timedelta(days=1, hours=10, minutes=15, seconds=1)
+            print (t1.total_seconds())
         """)
