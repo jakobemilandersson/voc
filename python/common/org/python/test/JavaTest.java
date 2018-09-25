ec7b63241238bc327b5bf5cc45b85b76aac01ff7
erikajansson1 <erika_jansson1@hotmail.com>
2018-09-25 14:54:23 +0200

    more tests

--- python/common/org/python/test/JavaTest.java
+++ python/common/org/python/test/JavaTest.java
@@ -3,6 +3,8 @@ package org.python.test;
 import org.python.types.List;
 import org.python.types.Int;
 import org.python.types.Object;
+import org.python.types.Bool;
+import org.python.types.Str;
 import java.util.*;
 
 import junit.framework.TestCase;
@@ -12,11 +14,23 @@ public class JavaTest extends TestCase {
     public void test_creation() {
         List x = new List();
         assertTrue(x.__len__() == Int.getInt(0));
-        // org.python.types.Object g = Int.getInt(7);
-        // org.python.types.Object h = Int.getInt(2);
-        // x.__add__(g);
-        // x.__add__(h);
-        // boolean n = x.__contains__(g);
-        // assertTrue(n == true);
+    }
+
+    public void test_append() {
+        List x = new List();
+
+        // Appending items
+        Object firstInt = Int.getInt(7);
+        Object secondInt = Int.getInt(2);
+        x.append(firstInt);
+        x.append(secondInt);
+        Bool firstIntExist = (Bool) x.__contains__(firstInt);
+        assertTrue(firstIntExist.value);
+
+        // Testappending mixed types in list
+        Str s = new Str("hello");
+        x.append(s);
+        Bool strInList = (Bool) x.__contains__(s);
+        assertTrue(strInList.value && firstIntExist.value);
     }
 }
