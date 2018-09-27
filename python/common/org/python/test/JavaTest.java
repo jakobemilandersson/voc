5e5ff0aaccb6c5913f6017dba40b8b5111f3e2e8
Jakob Andersson <jaan0006@student.uu.se>
2018-09-27 15:07:25 +0200

    Added tests for pop and insert

--- python/common/org/python/test/JavaTest.java
+++ python/common/org/python/test/JavaTest.java
@@ -336,6 +336,109 @@ public class JavaTest extends TestCase {
 
         assertEquals(original, validation);
     }
+	
+	@Test
+    public void testListPopNoArg() {
+        List original = new List();
+        original.append(Int.getInt(1));
+        original.append(Int.getInt(2));
+        original.append(Int.getInt(3));
+        original.pop();
+
+        List validation = new List();
+        validation.append(Int.getInt(1));
+        validation.append(Int.getInt(2));
+
+        assertEquals(original, validation);
+    }
+
+    @Test
+    public void testListPopPosArg() {
+        List original = new List();
+        original.append(Int.getInt(1));
+        original.append(Int.getInt(2));
+        original.append(Int.getInt(3));
+        original.pop(0);
+
+        List validation = new List();
+        validation.append(Int.getInt(2));
+        validation.append(Int.getInt(3));
+
+        assertEquals(original, validation);
+    }
+
+    @Test 
+    public void testListPopNegArg() {
+        List original = new List();
+        original.append(Int.getInt(1));
+        original.append(Int.getInt(2));
+        original.append(Int.getInt(3));
+        original.pop(-2);
+
+        List validation = new List();
+        validation.append(Int.getInt(2));
+        validation.append(Int.getInt(3));
+
+        assertEquals(original, validation);
+    }
+
+    @Test(expected = IndexError.class)
+    public void testListPopExceptionEmpty() {
+        List original = new List();
+        original.pop();
+    }
+
+    @Test(expected = IndexError.class)
+    public void testListPopExceptionIndOver() {
+        List original = new List();
+        original.append(Int.getInt(1));
+        original.append(Int.getInt(2));
+        original.append(Int.getInt(3));
+        original.pop(3);
+    }
+
+    @Test(expected = IndexError.class)
+    public void testListPopExceptionIndUnder() {
+        List original = new List();
+        original.append(Int.getInt(1));
+        original.append(Int.getInt(2));
+        original.append(Int.getInt(3));
+        original.pop(-4);
+    }
+
+    @Test
+    public void testListInsertInt() {
+        List original = new List();
+        original.append(Int.getInt(1));
+        original.append(Int.getInt(2));
+        original.append(Int.getInt(3));
+        original.insert(0, Int.getInt(4))
+
+        List validation = new List();
+        validation.append(Int.getInt(4));
+        validation.append(Int.getInt(1));
+        validation.append(Int.getInt(2));
+        validation.append(Int.getInt(3));
+
+        assertEquals(original, validation);
+    }
+
+    @Test
+    public void testListInsertStr() {
+        List original = new List();
+        original.append(Int.getInt(1));
+        original.append(Int.getInt(2));
+        original.append(Int.getInt(3));
+        original.insert(0, new Str("hello"))
+
+        List validation = new List();
+        validation.append(new Str("hello"));
+        validation.append(Int.getInt(1));
+        validation.append(Int.getInt(2));
+        validation.append(Int.getInt(3));
+
+        assertEquals(original, validation);
+    }
 
     // -----------------------------------------------------------
 
