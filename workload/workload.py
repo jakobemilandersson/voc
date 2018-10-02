11a4d809fcfb81f40edbd8a683ddf886e20f94d8
Henrik Thorsell <henrik.thorsell.4407@student.uu.se>
2018-10-02 14:15:38 +0200

    initial commit

---
+++ workload/workload.py
@@ -0,0 +1,36 @@
+import time
+
+def stop_timer(start_time):
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+def workload_append():
+    start_time = time.time()
+
+    list = [1]
+
+    for x in range(0, 1000*1000*10):
+        list.append(x)
+
+    stop_timer(start_time)
+
+def workload_insert():
+    start_time = time.time()
+
+    list = [1]
+    for x in range(0, 1000*1000*5):
+        list.insert(x, x)
+
+    stop_timer(start_time)
+
+def workload_sort():
+    start_time = time.time()
+
+    list = [1]
+    for x in range(0, 1000*1000*10)
+        list.insert()
+
+def workload_count():
+    start_time = 
+
+workload_append()
+workload_sort()
