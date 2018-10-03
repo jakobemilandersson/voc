ee5f7f0ecfb11e3fb429e8a08ab8cd480c5da165
Jakob Andersson <jaan0006@student.uu.se>
2018-10-03 12:50:16 +0200

    Fixing small merge-error

--- python/common/org/python/workloadjava/Workload.java
+++ python/common/org/python/workloadjava/Workload.java
@@ -1,4 +1,29 @@
 package org.python.workloadjava;
+import org.python.types.Int;
+import org.python.types.List;
 
 public class Workload {
+
+    public static void workloadAppend() {
+        List list = new List();
+
+        for(int i = 0; i < 1000*1000*1; i++) {
+            list.append(Int.getInt(i));
+        }
+    }
+
+    public static void workloadInsert() {
+        List list = new List();
+
+        for(int i = 0; i < 1000*1000*1; i++) {
+            list.insert(Int.getInt(i), Int.getInt(i));
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println("YAY");
+        Workload.workloadAppend();
+        Workload.workloadInsert();
+        System.out.println("YAhiuyghY");
+    }
 }
